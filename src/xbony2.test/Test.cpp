f3f214306d839e11e35a18a0875369d2d9ca8721
xbony2 <xbony22@gmail.com>
2014-12-04 20:01:12 -0500

    Should work :P (I'm 100% new to C++)

---
+++ src/xbony2.test/Test.cpp
@@ -0,0 +1,14 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: Dec 4, 2014
+ *      Author: xbony2
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout >> "xbonys iz awesome" << endl;
+}
