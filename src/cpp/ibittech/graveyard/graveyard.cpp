016ec675e03845b4504917f5c9f9802ec8154a6d
xbony2 <xbony22@gmail.com>
2014-12-04 20:58:04 -0500

    Changed test file to fit C++ convension, created the base file for graveyard.

---
+++ src/cpp/ibittech/graveyard/graveyard.cpp
@@ -0,0 +1,18 @@
+/*
+ * graveyard.cpp
+ *
+ *  Created on: Dec 4, 2014
+ *      Author: xbony2
+ */
+
+#include "graveyard.h"
+
+graveyard::graveyard() {
+	// TODO Auto-generated constructor stub
+
+}
+
+graveyard::~graveyard() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/cpp/ibittech/graveyard/graveyard.h
@@ -0,0 +1,17 @@
+/*
+ * graveyard.h
+ *
+ *  Created on: Dec 4, 2014
+ *      Author: xbony2
+ */
+
+#ifndef GRAVEYARD_H_
+#define GRAVEYARD_H_
+
+class graveyard {
+public:
+	graveyard();
+	virtual ~graveyard();
+};
+
+#endif /* GRAVEYARD_H_ */
